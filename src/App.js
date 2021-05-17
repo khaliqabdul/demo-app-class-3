272c492c401ceb3b064441725ba4b0fce4224b70
Abdul Khaliq <abdulkhaliqazeemi@gmail.com>
2021-05-17 06:05:59 +0500

    updated app

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App(){
     {/* This is comment tag*/}
     <Dinner title="Breakfast" dishName="'Anda(eag) Paratha'" sweetDish="'Green Tea'" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRnz9861qmgV5UJxIXuRbZIo04CUUvEX4KrQ&usqp=CAU" />
     <hr />
-    <Dinner title="Lunch" dishName="'Nihari'" sweetDish="'Gajir ka Halwa'" imageUrl="" />
+    <Dinner title="Lunch" dishName="'Nihari'" sweetDish="'Gajir ka Halwa'" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRnz9861qmgV5UJxIXuRbZIo04CUUvEX4KrQ&usqp=CAU" />
     <hr/>
-    <Dinner title="Dinner" dishName="'Sabzi Pulao'" sweetDish="'Kheer'" imageUrl="" />
+    <Dinner title="Dinner" dishName="'Sabzi Pulao'" sweetDish="'Kheer'" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRnz9861qmgV5UJxIXuRbZIo04CUUvEX4KrQ&usqp=CAU" />
     </header>
     </div>
   );
